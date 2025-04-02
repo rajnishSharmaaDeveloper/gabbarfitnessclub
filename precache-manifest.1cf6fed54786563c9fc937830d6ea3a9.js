cbdebd99dec22a5cde5f37194c0ac2e05dfd4dc2
rajnish sharma <rajnish.softfix@gmail.com>
2025-04-02 11:19:22 +0530

    Updates

---
+++ precache-manifest.1cf6fed54786563c9fc937830d6ea3a9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da25c1e1a2f8350eeddcf5f0209d22c",
+    "url": "/gabbarfitnessclub/index.html"
+  },
+  {
+    "revision": "794ff9ac43dca029559b",
+    "url": "/gabbarfitnessclub/static/css/main.de31fb63.chunk.css"
+  },
+  {
+    "revision": "70cfd9d7c9cdaf22a924",
+    "url": "/gabbarfitnessclub/static/js/2.4cbe7d39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gabbarfitnessclub/static/js/2.4cbe7d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794ff9ac43dca029559b",
+    "url": "/gabbarfitnessclub/static/js/main.841da22e.chunk.js"
+  },
+  {
+    "revision": "ae1c425ce5dbe13f643b",
+    "url": "/gabbarfitnessclub/static/js/runtime-main.561724af.js"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/gabbarfitnessclub/static/media/bgVideo.ae250877.mp4"
+  },
+  {
+    "revision": "dd1df8f478bb28433b5dfc6df4832f09",
+    "url": "/gabbarfitnessclub/static/media/classFour.dd1df8f4.jpg"
+  },
+  {
+    "revision": "5a6c841c01f957d21afe705bf1754ce0",
+    "url": "/gabbarfitnessclub/static/media/classOne.5a6c841c.jpg"
+  },
+  {
+    "revision": "3d605f688128d539f1898c2c8867814b",
+    "url": "/gabbarfitnessclub/static/media/classThree.3d605f68.jpg"
+  },
+  {
+    "revision": "a6ebee97d6a29927f91b2a238d1b8336",
+    "url": "/gabbarfitnessclub/static/media/classTwo.a6ebee97.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/gabbarfitnessclub/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/gabbarfitnessclub/static/media/gymOverlayBg.f042c2ac.jpg"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/gabbarfitnessclub/static/media/scheduleBg.02e36482.jpg"
+  },
+  {
+    "revision": "7c2ab25d46f7a6f96ce234d934f56a80",
+    "url": "/gabbarfitnessclub/static/media/trainerOne.7c2ab25d.jpg"
+  },
+  {
+    "revision": "81e56a6dd10134e1affed0e5f247541f",
+    "url": "/gabbarfitnessclub/static/media/trainerThree.81e56a6d.jpg"
+  },
+  {
+    "revision": "db6c16448e4272b3e4eb88cdc84c356f",
+    "url": "/gabbarfitnessclub/static/media/trainerTwo.db6c1644.jpg"
+  }
+]);
